30cc8590ce3be2bbf81a1fc24643e3be0d9f69dc
marcelinho21junior <msbcontjr@outlook.com>
2021-12-27 11:05:43 -0300

    commit atualizado 21

--- page/js/app.js
+++ page/js/app.js
@@ -1,7 +1,7 @@
 var btnSignin = document.querySelector('#signin');
 var btnSignup = document.querySelector('#signup');
 
-var body = document.getElementById('body');
+var body = document.querySelector("body");
 
 btnSignin.addEventListener('click', function(){
     body.className = 'sign-in-js'
